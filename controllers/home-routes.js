3aad36b96a5f91ee7fe1dc7f085d8f61e829b54a
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-06-12 17:56:20 -0600

    added race page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+//Renders Homepage
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -19,6 +20,7 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+//Renders Login Page
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
@@ -27,5 +29,22 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+router.get('/race', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password', 'email'] },
+      });
+  
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      res.render('race', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
