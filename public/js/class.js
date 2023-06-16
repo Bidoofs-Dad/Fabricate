5cf1bf4640b6db7943713d8fea9daa109fa62ec7
Zane Werle <zanewerle@gmail.com>
2023-06-16 18:59:11 -0500

    added in the script that reads which class you choose and saves it to the local storage, it is then read on the 'characterSheet' page and placed into its spot

---
+++ public/js/class.js
@@ -0,0 +1,13 @@
+const classButtons = document.querySelectorAll('.class-button');
+
+classButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+
+        const classChoice = button.querySelector('p').textContent;
+        
+        console.log(classChoice); 
+
+        localStorage.setItem('classChoice', classChoice);
+    });
+});
+
