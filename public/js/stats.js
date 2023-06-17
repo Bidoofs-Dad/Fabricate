02dcd78191d6905eeaba686d406d416a0e51ca82
Zane Werle <zanewerle@gmail.com>
2023-06-16 20:38:32 -0500

    added in the script for the rest of the stats (dex, const, intel, wis and char) now when getting to the charcacter sheet, race, class, background and all 6 stats populate with your choices thanks to local storage!

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,10 +1,25 @@
 const statsSubmitBtn = document.getElementById('stats-submit');
 const strengthEl = document.getElementById('strength');
+const dexterityEl = document.getElementById('dexterity');
+const constitutionEl = document.getElementById('constitution');
+const intelligenceEl = document.getElementById('intelligence');
+const wisdomEl = document.getElementById('wisdom');
+const charismaEl = document.getElementById('charisma');
 
 const statsSubmit = () => {
     const strengthVal = strengthEl.value
+    const dexterityVal = dexterityEl.value
+    const constitutionVal = constitutionEl.value
+    const intelligenceVal = intelligenceEl.value
+    const wisdomVal = wisdomEl.value
+    const charismaVal = charismaEl.value
 
        console.log(strengthVal);
 
        localStorage.setItem('strengthVal', strengthVal);
+       localStorage.setItem('dexterityVal', dexterityVal);
+       localStorage.setItem('constitutionVal', constitutionVal);
+       localStorage.setItem('intelligenceVal', intelligenceVal);
+       localStorage.setItem('wisdomVal', wisdomVal);
+       localStorage.setItem('charismaVal', charismaVal);
 }
